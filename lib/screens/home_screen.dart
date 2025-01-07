566dfd71d6d68eace07de807fa970fa0694986dc
Dwidenbrahma <dwidwnbrahmaa222@gmail.com>
2025-01-07 23:15:12 +0530

    updating the homepage of the attendify

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:attendify/screens/register_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:attendify/constant.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Attendify extends StatefulWidget {
   static const id = 'home_screen';
@@ -18,13 +19,87 @@ class _AttendifyState extends State<Attendify> {
     return SafeArea(
       child: Scaffold(
         body: Container(
+          width: double.infinity,
+          height: double.infinity,
           decoration: BoxDecoration(
             image: DecorationImage(
               image: AssetImage('assets/images/homepage.png'),
               fit: BoxFit.cover,
             ),
           ),
-          child: Column(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Text(
+                    'Welcome to ',
+                    style: TextStyle(
+                      fontSize: 40.0,
+                      color: Color(0xFF000957),
+                      fontFamily: 'Roboto',
+                    ),
+                  ),
+                  Text(
+                    'Attendify',
+                    textAlign: TextAlign.right,
+                    style: TextStyle(
+                      fontSize: 40.0,
+                      color: Color(0xFF000957),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  SizedBox(
+                    width: 150.0,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          padding: EdgeInsets.all(10.0),
+                        backgroundColor: Color(0xFF000000),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                              10), // Button border radius (rounded corners)
+                        ),
+                      ),
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          fontSize: 25.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                  width: 150.0,
+                    child: ElevatedButton(
+                      
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.all(10.0),
+                        backgroundColor: Color(0xFF000000),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                              10), // Button border radius (rounded corners)
+                        ),
+                      ),
+                      child: Text(
+                        'Sign Up',
+                        style: TextStyle(
+                          fontSize: 25.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
