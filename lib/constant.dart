980d64860d0ef3eb96565ba627be3198784666b6
Dwidenbrahma <dwidwnbrahmaa222@gmail.com>
2025-01-07 21:43:27 +0530

    updating the Route for different screen

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,5 +1,17 @@
-import 'dart:ui';
+import 'package:flutter/material.dart';
 
 // String kFontFamily = TextStyle(
 //   color: Color(),
 // );
+
+const colorizeColors = [
+  Colors.purple,
+  Colors.blue,
+  Colors.yellow,
+  Colors.red,
+];
+
+const colorizeTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontFamily: 'Horizon',
+);
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:attendify/screens/login_screen.dart';
 import 'package:attendify/screens/register_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:attendify/constant.dart';
 
 class Attendify extends StatefulWidget {
   static const id = 'home_screen';
@@ -15,28 +17,14 @@ class _AttendifyState extends State<Attendify> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextButton(
-              onPressed: () {
-                Navigator.pushNamed(context, LoginScreen.id);
-              },
-              child: Text(
-                'Login',
-                style: TextStyle(fontSize: 30.0),
-              ),
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/homepage.png'),
+              fit: BoxFit.cover,
             ),
-            TextButton(
-              onPressed: () {
-                Navigator.pushNamed(context, RegisterScreen.id);
-              },
-              child: Text(
-                'Register',
-                style: TextStyle(fontSize: 30.0),
-              ),
-            ),
-          ],
+          ),
+          child: Column(),
         ),
       ),
     );
